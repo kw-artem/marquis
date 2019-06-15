83491af01501d3de25ae9a146f18e9f4d5ac8ab8
Artem Zablodskiy <kw-artem@yandex.ru>
2019-06-15 15:37:05 +0300

    Init project

---
+++ src/main/java/cf/creativeflow/marquis/blog/MarquisApplication.java
@@ -0,0 +1,13 @@
+package cf.creativeflow.marquis.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarquisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarquisApplication.class, args);
+	}
+
+}
