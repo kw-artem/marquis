e1b013eb89cfac79b517013627297796d40e9af1
Artem Zablodskiy <kw-artem@yandex.ru>
2019-06-16 11:56:54 +0300

    Изменена реализация Post

--- src/main/java/cf/creativeflow/marquis/blog/controller/PostController.java
+++ src/main/java/cf/creativeflow/marquis/blog/controller/PostController.java
@@ -3,34 +3,79 @@ package cf.creativeflow.marquis.blog.controller;
 import cf.creativeflow.marquis.blog.domain.Post;
 import cf.creativeflow.marquis.blog.exceptions.PostNotFoundException;
 import cf.creativeflow.marquis.blog.repository.PostRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
 
     private final PostRepository postRepository;
+    private final ObjectMapper objectMapper;
 
-    PostController(PostRepository postRepository){
+    PostController(PostRepository postRepository, ObjectMapper objectMapper){
         this.postRepository = postRepository;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping
     public List<Post> getAllPosts(){
+
         return postRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Post getPostById(@PathVariable Long id){
+
         return postRepository.findById(id)
                 .orElseThrow(() -> new PostNotFoundException(id));
     }
 
     @PostMapping
     public Post createPost(@RequestBody Post post){
+
+        Date creationDate = new Date();
+        post.setCreationDate(creationDate);
+        post.setLastChangeDate(creationDate);
+        System.out.println(post);
+
         return postRepository.save(post);
+    }
+
+    @PutMapping("/{id}")
+    public Post fullUpdatePostById(@PathVariable Long id, @RequestBody Post modifiedPost){
+
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new PostNotFoundException(id));
+        BeanUtils.copyProperties(modifiedPost, post, "id");
+        post.setLastChangeDate(new Date());
+
+        return postRepository.save(post);
+    }
+
+    @PatchMapping("/{id}")
+    public Post partialUpdatePostById(@PathVariable Long id){
+
+        return postRepository.findById(id)
+                .orElseThrow(() -> new PostNotFoundException(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable Long id){
+
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new PostNotFoundException(id));
+
+        postRepository.delete(post);
     }
 
 }
--- src/main/java/cf/creativeflow/marquis/blog/domain/Post.java
+++ src/main/java/cf/creativeflow/marquis/blog/domain/Post.java
@@ -1,6 +1,7 @@
 package cf.creativeflow.marquis.blog.domain;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.Objects;
 
 @Entity
@@ -9,14 +10,46 @@ public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(updatable = false)
     private Long id;
 
+    @Column(nullable = false)
+    private String title;
+
     private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_dt", updatable = false, nullable = false)
+    private Date creationDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "changed_dt")
+    private Date lastChangeDate;
+
+    //private String description;
+
+    private String tags;
 
     public Post() {}
 
     public Long getId() {
         return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
     }
 
     public void setId(Long id) {
@@ -31,6 +64,22 @@ public class Post {
         this.content = content;
     }
 
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastChangeDate() {
+        return lastChangeDate;
+    }
+
+    public void setLastChangeDate(Date lastChangeDate) {
+        this.lastChangeDate = lastChangeDate;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -42,5 +91,17 @@ public class Post {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", creationDate=" + creationDate +
+                ", lastChangeDate=" + lastChangeDate +
+                ", tags='" + tags + '\'' +
+                '}';
     }
 }
